ae9214cc25fff43142770e2f6ba4252043ec40c3
Muhammad Umer <42178214+umershaikh123@users.noreply.github.com>
2022-09-07 21:33:43 +0500

    Update smartContract.sol

--- Remix-Contracts/Moralis Smart Contract/smartContract.sol
+++ Remix-Contracts/Moralis Smart Contract/smartContract.sol
@@ -74,7 +74,7 @@ import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.s
 import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
-contract SoulBoundTest is ERC721, ERC721URIStorage, Ownable {
+contract SoulBoundContract is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
